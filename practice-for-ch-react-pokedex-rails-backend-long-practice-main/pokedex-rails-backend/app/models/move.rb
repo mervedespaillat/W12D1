f8f1a2300637f3664e9cff49f45cfcae0e18f367
carveyh <carvey.hor@gmail.com>
2023-05-22 17:05:56 -0400

    Seed complete

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/move.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/move.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: moves
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Move < ApplicationRecord
+	validates :name, length: { maximum: 255 }, uniqueness: { message: "Name must be unique, '%{value}' already taken" }
+
+	has_many :poke_moves,
+		foreign_key: :move_id,
+		class_name: :PokeMove,
+		dependent: :destroy
+
+	has_many :pokemons,
+		through: :poke_moves,
+		source: :pokemon
 end
--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/poke_move.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/poke_move.rb
@@ -1,4 +1,16 @@
+# == Schema Information
+#
+# Table name: poke_moves
+#
+#  id         :bigint           not null, primary key
+#  pokemon_id :bigint           not null
+#  move_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class PokeMove < ApplicationRecord
+  validates :pokemon_id, uniqueness: { scope: :move_id, message: "Pick another move. Please." }
+
   belongs_to :pokemon
   belongs_to :move
 end
